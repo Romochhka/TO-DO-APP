093a31b5284bd964310e3cef4008146c8be4a66e
Ramzat Ruslanov <ruslanov595@gmail.com>
2025-01-11 13:54:13 +0500

    Finished the application. I've been working with the data.Used LocalStorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
 const TasksList = document.querySelector('#tasksList');
-const EmptyList = document.querySelector('#emptyList');
+const emptyList = document.querySelector('#emptyList');
+
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach((task) => renderTask(task));
+
+}
+
+
+checkEmptyList();
 
 form.addEventListener('submit', addTask)
 
@@ -9,20 +21,94 @@ TasksList.addEventListener('click', deleteTask)
 
 TasksList.addEventListener('click', doneTask)
 
-function addTask (event) {
+function addTask(event) {
     event.preventDefault();
 
     const taskText = taskInput.value;
 
-    // Проверка на пустую строку
-    if (!taskText.trim()) {
-        console.log("Задача не может быть пустой");
-        return;
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false
+    };
+
+    tasks.push(newTask)
+
+    saveToLocalStorage();
+    renderTask (newTask);
+    taskInput.value = '';
+    taskInput.focus();
+
+    checkEmptyList();
+}
+
+function deleteTask(event) {
+
+    if (event.target.dataset.action !== 'delete') return;
+
+
+    const parenNode = event.target.closest('li');
+
+    const id = Number(parenNode.id);
+
+    tasks = tasks.filter((task) => task.id !== id)
+
+    saveToLocalStorage();
+
+    parenNode.remove();
+    checkEmptyList();
+
+
+}
+
+function doneTask(event) {
+
+    if (event.target.dataset.action !== 'done') return;
+
+    const parentNode = event.target.closest('li');
+
+
+    const id = Number(parentNode.id);
+    const task = tasks.find((task) => task.id === id);
+    task.done = !task.done;
+
+    saveToLocalStorage();
+
+    const taskTitle = parentNode.querySelector('span');
+    taskTitle.classList.toggle('task-title--done');
+    console.log(taskTitle);
+
+}
+
+function checkEmptyList() {
+    if (tasks.length === 0) {
+        const emptyListElement = `
+    <li id="emptyList" class="list-group-item empty-list">
+        <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+        <div class="empty-list__title">Список дел пуст</div>
+    </li>`;
+
+        TasksList.insertAdjacentHTML(`afterbegin`, emptyListElement);
     }
 
+    if (tasks.length > 0) {
+        const emptyListEl = document.querySelector('#emptyList');
+        emptyListEl ? emptyListEl.remove() : null;
+    }
+
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask (task) {
+    const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+
+
     const taskHTML = `
-        <li class="list-group-item d-flex justify-content-between task-item">
-            <span class="task-title">${taskText}</span>
+        <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+            <span class="${cssClass}">${task.text}</span>
             <div class="task-item__buttons">
                 <button type="button" data-action="done" class="btn-action">
                     <img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -34,29 +120,4 @@ function addTask (event) {
         </li>`;
 
     TasksList.insertAdjacentHTML('beforeend', taskHTML);
-    taskInput.value = '';
-    taskInput.focus();
-    if (TasksList.children.length > 1 ) {
-        EmptyList.classList.add('none');
-    }
-}
-
-function deleteTask (event) {
-
-    if (event.target.dataset.action === 'delete') {
-        event.target.closest('li').remove();
-    }
-
-    if (TasksList.children.length === 1 ) {
-        EmptyList.classList.remove('none');
-    }
-}
-
-function doneTask (event) {
-    if (event.target.dataset.action === 'done') {
-       const parentNode = event.target.closest('li');
-       const taskTitle= parentNode.querySelector('span');
-       taskTitle.classList.toggle('task-title--done');
-       console.log(taskTitle);
-    }
 }
